d99b1f2ad0dbaf21d320ed33908ac3c8922d146c
Philippe Meurant <philippe.meurant@mbseconsulting.com>
2025-11-11 11:57:37 +0100

    Update magicforge plugin version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `maven-publish`
     id("com.diffplug.spotless") version "7.2.1" // https://github.com/diffplug/spotless
     id("jacoco") // https://docs.gradle.org/current/userguide/jacoco_plugin.html
-    id("com.mbseconsulting.magicforge") version "1.0.0-SNAPSHOT" // https://github.com/mbseconsulting/magic-forge
+    id("com.mbseconsulting.magicforge") version "1.0.0" // https://github.com/mbseconsulting/magic-forge
 }
 
 repositories {
